db0a8f4f8272ab7e662baf04c6dc93e2e0aa64f6
Ignacio Picotto <ignacio.picotto@claro.com.ar>
2018-09-18 17:05:18 -0300

    Con Hystrix.

--- csc-business-service/src/main/java/com/claro/amx/service/impl/ServiceImpl.java
+++ csc-business-service/src/main/java/com/claro/amx/service/impl/ServiceImpl.java
@@ -1,5 +1,6 @@
 package com.claro.amx.service.impl;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.claro.amx.dao.ConsumerSvcClient;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class ServiceImpl {
@@ -15,12 +17,23 @@ public class ServiceImpl {
 	@Autowired
 	ConsumerSvcClient consumerSvcClient;
 	
+	@HystrixCommand(fallbackMethod = "getPruebaDefault")
 	@RequestMapping(value = "/prueba", method = RequestMethod.GET)
 	public Map<String, String> getPrueba() {
 		
 		Map<String, String> mapa = consumerSvcClient.message();
 		
 		mapa.put("business-message", "Mensaje de la capa Business.");
+		
+		return mapa;
+		
+	}
+
+	public Map<String, String> getPruebaDefault() {
+		
+		Map<String, String> mapa = new HashMap<>();
+		
+		mapa.put("business-message", "ERROR!!!!!.");
 		
 		return mapa;
 		
--- csc-business-service/src/main/java/com/claro/amx/start/ServletInitializer.java
+++ csc-business-service/src/main/java/com/claro/amx/start/ServletInitializer.java
@@ -2,6 +2,7 @@ package com.claro.amx.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = {"com.claro.amx"})
+@EnableCircuitBreaker
 @ComponentScan("com.claro.amx")
 public class ServletInitializer {
     public static void main(String[] args) {
